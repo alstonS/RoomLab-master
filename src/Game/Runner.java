3b1d41f92c9057d3e702b4469f09afc4ed36859a
BT_1N3_33 <ashi3454@bths.edui>
2018-11-01 10:16:48 -0400

    1

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -30,13 +30,15 @@ public class Runner {
 		int x = (int)(Math.random()*building.length);
 		int y = (int)(Math.random()*building.length);
 		building[x][y] = new WinningRoom(x, y);
-		while ( int j > 0)
+		while (int j >= 0)
 		{
 			int a = (int) (Math.random() * building.length);
 			int b = (int) (Math.random() * building.length);
+			dingowde
 			j--;
 		}
-		building[a][b] = new emptyRoom(a, b);
+
+
 	//Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
 		building[0][0].enterRoom(player1);
